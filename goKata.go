76c20a17df3b2c3517698a657b97bd0758495545
Roy Dybing <roy.dybing@gmail.com>
2016-09-19 10:50:20 +0200

    Do the FIzzBuzz

--- goKata.go
+++ goKata.go
@@ -1,3 +1,5 @@
+// Various small katas to test out Go
+
 package main
 
 import (
@@ -5,5 +7,22 @@ import (
 )
 
 func main() {
-	fmt.Println("goKata test")
+
+	var text string
+
+	for i := 1; i <= 100; i++ {
+		// when mod of i/x is zero...
+		switch 0 {
+		case i % 15:
+			text = "FizzBuzz"
+			break
+		case i % 5:
+			text = "Fizz"
+		case i % 3:
+			text = "Buzz"
+		default:
+			text = ""
+		}
+		fmt.Printf("%v : %s\n", i, text)
+	}
 }
